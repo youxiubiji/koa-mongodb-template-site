b409a9756ff7d7caacec45005d570345b405ddbf
徐梦强 <youxiubiji@gmail.com>
2022-01-07 14:20:52 +0800

    修改：部署脚本

--- .temp/app-enhancers/1.js
+++
@@ -1,2 +0,0 @@
-import "C:\\D\\koa-mongodb-template-site\\node_modules\\@vuepress\\plugin-nprogress\\enhanceAppFile.js"
-export default {}
--- .temp/app-enhancers/global-components-9.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-Vue.component("Badge", () => import("C:\\D\\koa-mongodb-template-site\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
-Vue.component("CodeBlock", () => import("C:\\D\\koa-mongodb-template-site\\node_modules\\@vuepress\\theme-default\\global-components\\CodeBlock"))
-Vue.component("CodeGroup", () => import("C:\\D\\koa-mongodb-template-site\\node_modules\\@vuepress\\theme-default\\global-components\\CodeGroup"))
-
-
-export default {}
